eca8e540776b2131d2929bc346a7a2fa89f5f28e
k-poschmann <kristin.poschmann@hs-furtwangen.de>
2020-05-08 21:49:10 +0200

    commit

--- Aufgabe_3/script.js
+++ Aufgabe_3/script.js
@@ -4,7 +4,7 @@ var Haushaltshilfe;
     window.addEventListener("load", handleLoad);
     // Die Variablen werden typisiert und deklariert
     let totalCost = 0;
-    let form = document.querySelector("div#form");
+    let form = document.querySelector("form");
     let selectshopping = document.querySelector("#selectshopping");
     let selecthelp = document.querySelector("#selecthelp");
     let selectmoney = document.querySelector("#selectmoney");
@@ -21,7 +21,7 @@ var Haushaltshilfe;
     function handleLoad(_event) {
         //console.log("Start");
         // Event-Listener werden auf alle Buttons gesetzt
-        confirm.addEventListener("click", handleChange);
+        //confirm.addEventListener("click", handleChange);
         btn1.addEventListener("click", handleChange);
         btn2.addEventListener("click", handleChange);
         btn3.addEventListener("click", handleChange);
@@ -33,11 +33,47 @@ var Haushaltshilfe;
         //Erstellen des FormData Elements
         let formData = new FormData(document.forms[0]);
         for (let entry of formData) {
-            let select = "[value='" + entry[1] + "']";
-            let item = document.querySelector(select);
-            let price = Number(item.getAttribute("price"));
-            console.log(price);
-            //list.innerHTML += item.value + " " + price + "  €  <br>" ;
+            let selector = "[value='" + entry[1] + "']";
+            let item = document.querySelector(selector);
+            let containershopping = document.createElement("div");
+            let containerplace = document.createElement("div");
+            let toDocontainer = document.createElement("div");
+            let moneycontainer = document.createElement("div");
+            let cashcontainer = document.createElement("div");
+            let deletebtn = document.createElement("button");
+            switch (entry[0]) {
+                case "Auswahl":
+                case "Menge":
+                    break;
+                case "Items":
+                    let itemPrice = Number(item.getAttribute("price"));
+                    let menge = Number(formData.get("Menge"));
+                    let market = String(formData.get("maerkte"));
+                    itemPrice = menge * itemPrice;
+                    let gesamt = itemPrice;
+                    containershopping.innerHTML = "<h4>Einkauf</h4>" + menge + " " + entry[1] + " " + itemPrice.toFixed(2) + " €" + " " + market;
+                    list.appendChild(containershopping);
+                    totalCost += itemPrice;
+                    form.reset();
+                    break;
+                case "toDo":
+                    let price = Number(item.getAttribute("price"));
+                    toDocontainer.innerHTML = "<h4>Haushaltshilfe</h4>" + entry[1] + price + " €";
+                    list.appendChild(toDocontainer);
+                    totalCost += price;
+                    form.reset();
+                    break;
+                case "money":
+                    let geld = Number(formData.get("money"));
+                    moneycontainer.innerHTML = "<h4>Geld abheben</h4>" + geld + " €";
+                    list.appendChild(moneycontainer);
+                    form.reset();
+                    break;
+                case "cash":
+                    let cash = String(item.getAttribute("value"));
+                    cashcontainer.innerHTML = "" + cash;
+                    list.appendChild(cashcontainer);
+            }
         }
     }
     function displayAmount(_event) {
--- Aufgabe_3/script.ts
+++ Aufgabe_3/script.ts
@@ -4,12 +4,12 @@ namespace Haushaltshilfe {
     // Die Variablen werden typisiert und deklariert
 
     let totalCost: number = 0;
-    let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+    let form: HTMLFormElement = <HTMLFormElement>document.querySelector("form");
     let selectshopping: HTMLInputElement = <HTMLInputElement>document.querySelector("#selectshopping");
     let selecthelp: HTMLInputElement = <HTMLInputElement>document.querySelector("#selecthelp");
     let selectmoney: HTMLInputElement = <HTMLInputElement>document.querySelector("#selectmoney");
     let confirm: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#confirm");
-    
+
     let einkauf: HTMLDivElement = <HTMLDivElement>document.querySelector("#einkauf");
     let hilfe: HTMLDivElement = <HTMLDivElement>document.querySelector("#hilfe");
     let geld: HTMLDivElement = <HTMLDivElement>document.querySelector("#geld");
@@ -24,14 +24,14 @@ namespace Haushaltshilfe {
 
     function handleLoad(_event: Event): void {
         //console.log("Start");
-        
+
         // Event-Listener werden auf alle Buttons gesetzt
-        confirm.addEventListener("click", handleChange);
+        //confirm.addEventListener("click", handleChange);
         btn1.addEventListener("click", handleChange);
         btn2.addEventListener("click", handleChange);
         btn3.addEventListener("click", handleChange);
         btn4.addEventListener("click", handleChange);
-        
+
     }
 
 
@@ -39,20 +39,67 @@ namespace Haushaltshilfe {
     function handleChange(_event: Event): void {
         //selektieren des Elements, wo am Ende alles 'reinkommt'
         let list: HTMLDivElement = <HTMLDivElement>document.querySelector("#list");
-        
+
         //Erstellen des FormData Elements
         let formData: FormData = new FormData(document.forms[0]);
         for (let entry of formData) {
-            let select: string = "[value='" + entry[1] + "']";
-            let item: HTMLInputElement = <HTMLInputElement>document.querySelector(select);
-            let price: number = Number(item.getAttribute("price"));
-            console.log(price);
+            let selector: string = "[value='" + entry[1] + "']";
+            let item: HTMLInputElement = <HTMLInputElement>document.querySelector(selector);
 
-            //list.innerHTML += item.value + " " + price + "  €  <br>" ;
-        
+            let containershopping = document.createElement("div");
+            let containerplace = document.createElement("div");
+            let toDocontainer = document.createElement("div");
+            let moneycontainer = document.createElement("div");
+            let cashcontainer = document.createElement("div");
+            let deletebtn = document.createElement("button");
+
+
+            switch (entry[0]) {
+                case "Auswahl":
+
+                case "Menge":
+                    break;
+                case "Items":
+                    let itemPrice: number = Number(item.getAttribute("price"));
+                    let menge: number = Number(formData.get("Menge"));
+                    let market: string = String(formData.get("maerkte"));
+
+                    itemPrice = menge * itemPrice;
+                    let gesamt: number = itemPrice; 
+                    
+                    
+                    containershopping.innerHTML = "<h4>Einkauf</h4>" + menge + " " + entry[1] + " " + itemPrice.toFixed(2) + " €" + " " + market;
+                    list.appendChild(containershopping);
+                    totalCost += itemPrice;
+                    form.reset();
+                    break;
+
+                case "toDo":
+                   let price: number = Number(item.getAttribute("price"));
+                   toDocontainer.innerHTML = "<h4>Haushaltshilfe</h4>" + entry[1] + price + " €";
+                   list.appendChild(toDocontainer);
+                   totalCost += price;
+                   form.reset();
+                   break;
+
+                case "money":
+                    let geld: number = Number(formData.get("money"));
+                    moneycontainer.innerHTML = "<h4>Geld abheben</h4>" + geld + " €";
+                    list.appendChild(moneycontainer);
+                    form.reset();
+                    break;
+
+                case "cash":
+                    let cash: string = String(item.getAttribute("value"));
+                    cashcontainer.innerHTML = "" + cash;
+                    list.appendChild(cashcontainer);
+            }
+
+
+
         }
 
-        
+
     }
 
     function displayAmount(_event: Event): void {
