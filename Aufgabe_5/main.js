f135784a41a57c74bae98a1db89f56ce463baf7e
k-poschmann <kristin.poschmann@hs-furtwangen.de>
2020-05-22 17:15:29 +0200

    commit

--- Aufgabe_5/main.js
+++ Aufgabe_5/main.js
@@ -1,23 +1,15 @@
 "use strict";
 var Haushaltshilfe5;
 (function (Haushaltshilfe5) {
-    window.addEventListener("load", async function () {
-        let totalCost = 0;
-        let form = document.querySelector("form");
-        // let selectshopping: HTMLInputElement = <HTMLInputElement>document.querySelector("#selectshopping");
-        // let selecthelp: HTMLInputElement = <HTMLInputElement>document.querySelector("#selecthelp");
-        // let selectmoney: HTMLInputElement = <HTMLInputElement>document.querySelector("#selectmoney");
-        // let confirm: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#confirm");
+    window.addEventListener("load", handleLoad);
+    let totalCost = 0;
+    let form = document.querySelector("#form");
+    let totalprice = document.querySelector("#totalprice");
+    async function handleLoad(_event) {
         let deletebtn = document.querySelector("button[type=reset]");
-        let totalprice = document.querySelector("#totalprice");
-        let submit = document.querySelector("button#submit");
-        // let bar: HTMLInputElement = <HTMLInputElement>document.querySelector("#bar");
-        // let paypal: HTMLInputElement = <HTMLInputElement>document.querySelector("#paypal");
-        // let ueberweisung: HTMLInputElement = <HTMLInputElement>document.querySelector("#ueberweisung");
-        //console.log("Start");
+        let submit = document.querySelector("#submit");
         let btn1 = document.querySelector("#btn1");
         let btn2 = document.querySelector("#btn2");
-        let btn3 = document.querySelector("#btn3");
         let btn4 = document.querySelector("#btn4");
         let response = await fetch("Data.json");
         let offer = await response.text();
@@ -26,78 +18,74 @@ var Haushaltshilfe5;
         // Event-Listener werden auf alle Buttons gesetzt
         btn1.addEventListener("click", handleChange);
         btn2.addEventListener("click", handleChange);
-        btn3.addEventListener("click", handleChange);
         btn4.addEventListener("click", handleChange);
         submit.addEventListener("click", sendOrder);
         deletebtn.addEventListener("click", deleteList);
-        async function sendOrder(_event) {
-            console.log("send order");
-            let formData = new FormData(form);
-            let query = new URLSearchParams(formData);
-            await fetch("DataStructure.html?" + query.toString());
-            alert("Order sent!");
-        }
-        function handleChange() {
-            //selektieren des Elements, wo am Ende alles 'reinkommt'
-            let list = document.querySelector("#list");
-            //Erstellen des FormData Elements
-            let formData = new FormData(document.forms[0]);
-            for (let entry of formData) {
-                let selector = "[value='" + entry[1] + "']";
-                let item = document.querySelector(selector);
-                let containershopping = document.createElement("div");
-                let toDocontainer = document.createElement("div");
-                let cashcontainer = document.createElement("div");
-                switch (entry[0]) {
-                    case "Menge":
-                        break;
-                    case "einkauf":
-                        let itemPrice = Number(item.getAttribute("price"));
-                        let menge = Number(formData.get("Menge"));
-                        let market = String(formData.get("Ort"));
-                        itemPrice = menge * itemPrice;
-                        containershopping.innerHTML = "" + menge + " " + entry[1] + " " + itemPrice.toFixed(2) + " €" + " " + market;
-                        list.appendChild(containershopping);
-                        totalCost += itemPrice;
-                        console.log(market);
-                        form.reset();
-                        break;
-                    case "hilfe":
-                        let price = Number(item.getAttribute("price"));
-                        toDocontainer.innerHTML = "<h4>Haushaltshilfe</h4>" + entry[1] + price + " €";
-                        list.appendChild(toDocontainer);
-                        totalCost += price;
-                        form.reset();
-                        break;
-                    case "bezahlung":
-                        let paypal = document.getElementById("Paypal");
-                        let bar = document.getElementById("Bar");
-                        let zahlungsart;
-                        if (bar.checked == true) {
-                            zahlungsart = "Bar";
-                            cashcontainer.innerHTML = zahlungsart + " bezahlen";
-                        }
-                        else if (paypal.checked == true) {
-                            zahlungsart = "Paypal";
-                            cashcontainer.innerHTML = "Mit " + zahlungsart + " zahlen";
-                        }
-                        else {
-                            zahlungsart = "Überweisung";
-                            cashcontainer.innerHTML = "Per " + zahlungsart + " bezahlen";
-                        }
-                        //cashcontainer.innerHTML = "" + zahlungsart;
-                        list.appendChild(cashcontainer);
-                        form.reset();
-                        break;
-                }
-                totalprice.innerHTML = "GESAMT: " + totalCost.toFixed(2) + " €";
-                list.appendChild(totalprice);
+    }
+    async function sendOrder() {
+        console.log("send order");
+        let formData = new FormData(document.forms[0]);
+        let query = new URLSearchParams(formData);
+        await fetch("index.html?" + query.toString());
+        alert("Order sent!");
+    }
+    function handleChange(_event) {
+        //selektieren des Elements, wo am Ende alles 'reinkommt'
+        let list = document.querySelector("#list");
+        //Erstellen des FormData Elements
+        let formData = new FormData(document.forms[0]);
+        for (let entry of formData) {
+            let selector = "[value='" + entry[1] + "']";
+            let item = document.querySelector(selector);
+            let containershopping = document.createElement("div");
+            let toDocontainer = document.createElement("div");
+            let cashcontainer = document.createElement("div");
+            switch (entry[0]) {
+                case "Menge":
+                    break;
+                case "einkauf":
+                    let itemPrice = Number(item.getAttribute("price"));
+                    let menge = Number(formData.get("Menge"));
+                    let market = String(formData.get("Ort"));
+                    itemPrice = menge * itemPrice;
+                    containershopping.innerHTML = menge + " " + entry[1] + " " + itemPrice.toFixed(2) + " €" + " " + market;
+                    list.appendChild(containershopping);
+                    totalCost += itemPrice;
+                    console.log(market);
+                    break;
+                case "hilfe":
+                    let price = Number(item.getAttribute("price"));
+                    toDocontainer.innerHTML = "<h4>Haushaltshilfe</h4>" + entry[1] + price + " €";
+                    list.appendChild(toDocontainer);
+                    totalCost += price;
+                    break;
+                case "bezahlung":
+                    let paypal = document.getElementById("Paypal");
+                    let bar = document.getElementById("Bar");
+                    let zahlungsart;
+                    if (bar.checked == true) {
+                        zahlungsart = "Bar";
+                        cashcontainer.innerHTML = zahlungsart + " bezahlen";
+                    }
+                    else if (paypal.checked == true) {
+                        zahlungsart = "Paypal";
+                        cashcontainer.innerHTML = "Mit " + zahlungsart + " zahlen";
+                    }
+                    else {
+                        zahlungsart = "Überweisung";
+                        cashcontainer.innerHTML = "Per " + zahlungsart + " bezahlen";
+                    }
+                    //cashcontainer.innerHTML = "" + zahlungsart;
+                    list.appendChild(cashcontainer);
+                    break;
             }
         }
-        function deleteList(_event) {
-            let mainlist = document.querySelector("div#list");
-            mainlist.innerHTML = "";
-        }
-    });
+        totalprice.innerHTML = "GESAMT: " + totalCost.toFixed(2) + " €";
+        list.appendChild(totalprice);
+    }
+    function deleteList(_event) {
+        let mainlist = document.querySelector("div#list");
+        mainlist.innerHTML = "";
+    }
 })(Haushaltshilfe5 || (Haushaltshilfe5 = {}));
 //# sourceMappingURL=main.js.map
--- Aufgabe_5/main.ts
+++ Aufgabe_5/main.ts
@@ -1,131 +1,119 @@
 namespace Haushaltshilfe5 {
-    window.addEventListener("load", async function () {
-        let totalCost: number = 0;
-        let form: HTMLFormElement = <HTMLFormElement>document.querySelector("form");
-        // let selectshopping: HTMLInputElement = <HTMLInputElement>document.querySelector("#selectshopping");
-        // let selecthelp: HTMLInputElement = <HTMLInputElement>document.querySelector("#selecthelp");
-        // let selectmoney: HTMLInputElement = <HTMLInputElement>document.querySelector("#selectmoney");
-        // let confirm: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#confirm");
+    window.addEventListener("load", handleLoad);
+    let totalCost: number = 0;
+    let form: HTMLFormElement = <HTMLFormElement>document.querySelector("#form");
+    let totalprice: HTMLLabelElement = <HTMLLabelElement>document.querySelector("#totalprice");
+
+
+
+    async function handleLoad(_event: Event): Promise<void> {
         let deletebtn: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button[type=reset]");
-        let totalprice: HTMLLabelElement = <HTMLLabelElement>document.querySelector("#totalprice");
-        let submit: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#submit");
-
-        // let bar: HTMLInputElement = <HTMLInputElement>document.querySelector("#bar");
-        // let paypal: HTMLInputElement = <HTMLInputElement>document.querySelector("#paypal");
-        // let ueberweisung: HTMLInputElement = <HTMLInputElement>document.querySelector("#ueberweisung");
-
-
-
-
-        //console.log("Start");
+        let submit: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#submit");
         let btn1: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#btn1");
         let btn2: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#btn2");
-        let btn3: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#btn3");
         let btn4: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#btn4");
 
         let response: Response = await fetch("Data.json");
         let offer: string = await response.text();
         let data: Data = JSON.parse(offer);
-
         generateContent(data);
-
         // Event-Listener werden auf alle Buttons gesetzt
         btn1.addEventListener("click", handleChange);
         btn2.addEventListener("click", handleChange);
-        btn3.addEventListener("click", handleChange);
         btn4.addEventListener("click", handleChange);
         submit.addEventListener("click", sendOrder);
         deletebtn.addEventListener("click", deleteList);
 
+    }
 
 
-        async function sendOrder(_event: Event): Promise<void> {
-            console.log("send order");
-            let formData: FormData = new FormData(form);
-            let query: URLSearchParams = new URLSearchParams(<any>formData);
-            await fetch("DataStructure.html?" + query.toString());
-            alert("Order sent!");
-        }
 
 
-        function handleChange(): void {
+    async function sendOrder(): Promise<void> {
+        console.log("send order");
+        let formData: FormData = new FormData(document.forms[0]);
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        await fetch("index.html?" + query.toString());
+        alert("Order sent!");
+    }
 
-            //selektieren des Elements, wo am Ende alles 'reinkommt'
-            let list: HTMLDivElement = <HTMLDivElement>document.querySelector("#list");
 
-            //Erstellen des FormData Elements
-            let formData: FormData = new FormData(document.forms[0]);
-            for (let entry of formData) {
-                let selector: string = "[value='" + entry[1] + "']";
-                let item: HTMLInputElement = <HTMLInputElement>document.querySelector(selector);
+    function handleChange(_event: Event): void {
 
-                let containershopping: HTMLDivElement = <HTMLDivElement>document.createElement("div"); 
-                let toDocontainer: HTMLDivElement = <HTMLDivElement>document.createElement("div");
-                let cashcontainer: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+        //selektieren des Elements, wo am Ende alles 'reinkommt'
+        let list: HTMLDivElement = <HTMLDivElement>document.querySelector("#list");
 
-                switch (entry[0]) {
-                    case "Menge":
-                        break;
-                    case "einkauf":
-                        let itemPrice: number = Number(item.getAttribute("price"));
-                        let menge: number = Number(formData.get("Menge"));
-                        let market: string = String(formData.get("Ort"));
+        //Erstellen des FormData Elements
+        let formData: FormData = new FormData(document.forms[0]);
 
-                        itemPrice = menge * itemPrice;
+        for (let entry of formData) {
+            let selector: string = "[value='" + entry[1] + "']";
+            let item: HTMLInputElement = <HTMLInputElement>document.querySelector(selector);
 
-                        containershopping.innerHTML = "" + menge + " " + entry[1] + " " + itemPrice.toFixed(2) + " €" + " " + market;
-                        list.appendChild(containershopping);
-                        totalCost += itemPrice;
-                        console.log(market);
-                        form.reset();
-                        break;
-                    case "hilfe":
-                        let price: number = Number(item.getAttribute("price"));
-                        toDocontainer.innerHTML = "<h4>Haushaltshilfe</h4>" + entry[1] + price + " €";
-                        list.appendChild(toDocontainer);
-                        totalCost += price;
-                        form.reset();
-                        break;
-                    case "bezahlung":
-                        let paypal: HTMLInputElement = <HTMLInputElement>document.getElementById("Paypal");
-                        let bar: HTMLInputElement = <HTMLInputElement>document.getElementById("Bar");
-                        let zahlungsart: string;
-                        if (bar.checked == true) {
-                            zahlungsart = "Bar";
-                            cashcontainer.innerHTML = zahlungsart + " bezahlen";
-                        } else if (paypal.checked == true) {
-                            zahlungsart = "Paypal";
-                            cashcontainer.innerHTML = "Mit " + zahlungsart + " zahlen";
-                        } else {
-                            zahlungsart = "Überweisung";
-                            cashcontainer.innerHTML = "Per " + zahlungsart + " bezahlen";
-                        }
+            let containershopping: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+            let toDocontainer: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+            let cashcontainer: HTMLDivElement = <HTMLDivElement>document.createElement("div");
 
-                        //cashcontainer.innerHTML = "" + zahlungsart;
-                        list.appendChild(cashcontainer);
-                        form.reset();
-                        break;
-                }
-                totalprice.innerHTML = "GESAMT: " + totalCost.toFixed(2) + " €";
-                list.appendChild(totalprice);
+            switch (entry[0]) {
+                case "Menge":
+                    break;
+                case "einkauf":
+                    let itemPrice: number = Number(item.getAttribute("price"));
+                    let menge: number = Number(formData.get("Menge"));
+                    let market: string = String(formData.get("Ort"));
+
+                    itemPrice = menge * itemPrice;
+
+                    containershopping.innerHTML = menge + " " + entry[1] + " " + itemPrice.toFixed(2) + " €" + " " + market;
+                    list.appendChild(containershopping);
+                    totalCost += itemPrice;
+                    console.log(market);
+                    break;
+                case "hilfe":
+                    let price: number = Number(item.getAttribute("price"));
+                    toDocontainer.innerHTML = "<h4>Haushaltshilfe</h4>" + entry[1] + price + " €";
+                    list.appendChild(toDocontainer);
+                    totalCost += price;
+                    break;
+                case "bezahlung":
+                    let paypal: HTMLInputElement = <HTMLInputElement>document.getElementById("Paypal");
+                    let bar: HTMLInputElement = <HTMLInputElement>document.getElementById("Bar");
+                    let zahlungsart: string;
+                    if (bar.checked == true) {
+                        zahlungsart = "Bar";
+                        cashcontainer.innerHTML = zahlungsart + " bezahlen";
+                    } else if (paypal.checked == true) {
+                        zahlungsart = "Paypal";
+                        cashcontainer.innerHTML = "Mit " + zahlungsart + " zahlen";
+                    } else {
+                        zahlungsart = "Überweisung";
+                        cashcontainer.innerHTML = "Per " + zahlungsart + " bezahlen";
+                    }
+
+                    //cashcontainer.innerHTML = "" + zahlungsart;
+                    list.appendChild(cashcontainer);
+                    break;
             }
-
-
+        
         }
 
-
-        function deleteList(_event: Event): void {
-                let mainlist: HTMLDivElement = <HTMLDivElement>document.querySelector("div#list");
-                mainlist.innerHTML = "";
-            
-        }
-
-    });
+        totalprice.innerHTML = "GESAMT: " + totalCost.toFixed(2) + " €";
+        list.appendChild(totalprice);
 
 
+    }
 
 
+    function deleteList(_event: Event): void {
+        let mainlist: HTMLDivElement = <HTMLDivElement>document.querySelector("div#list");
+        mainlist.innerHTML = "";
 
-
+    }
 
 }
+
+
+
+
+
+
