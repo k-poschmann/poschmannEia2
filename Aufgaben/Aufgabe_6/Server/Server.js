2e4d0e48d9aaf91e1e0fe939f672d2aba0cfe63c
k-poschmann <kristin.poschmann@hs-furtwangen.de>
2020-05-29 16:30:50 +0200

    commit

--- Aufgaben/Aufgabe_6/Server/Server.js
+++ Aufgaben/Aufgabe_6/Server/Server.js
@@ -18,6 +18,13 @@ var Haushaltshilfe_6;
         if (_request.url) {
             let url = Url.parse(_request.url, true);
             for (let key in url.query) {
+                // switch (key) {
+                //     case "einkauf":
+                //     break;
+                //     default:
+                //     _response.write(key + ":" + url.query[key] + "<br>");
+                //     break;
+                // }
                 _response.write(key + ":" + url.query[key] + "<br>");
             }
             let jsonString = JSON.stringify(url.query);
