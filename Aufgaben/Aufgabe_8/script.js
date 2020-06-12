718c1094586b6cf43bd3f563a85cb4b964afd2b8
k-poschmann <kristin.poschmann@hs-furtwangen.de>
2020-06-12 18:13:46 +0200

    commit

--- Aufgaben/Aufgabe_8/script.js
+++ Aufgaben/Aufgabe_8/script.js
@@ -5,6 +5,8 @@ var Virus;
     window.addEventListener("load", handleLoad);
     let canvas = document.querySelector("canvas");
     let crc2 = canvas.getContext("2d");
+    let width;
+    let height;
     function resizeWindow() {
         let myWidth = window.innerWidth - 5;
         let myHeight = window.innerHeight - 5;
@@ -12,14 +14,13 @@ var Virus;
         crc2.canvas.height = myHeight;
         drawBackground();
         crc2.fillRect(0, 0, myWidth, myHeight);
-        console.log(myWidth, myHeight);
     }
     function handleLoad(_event) {
         resizeWindow();
         drawBackground();
-        drawCells({ x: 500, y: 125 }, { x: 250, y: 75 });
-        drawCoronaVirus({ x: 0, y: 150 }, 30, 100);
-        drawAntibody();
+        drawCells();
+        drawCoronaVirus({ x: 200, y: 400 }, { x: 70, y: 70 });
+        drawAntibody({ x: 400, y: 300 }, { x: 100, y: 120 });
     }
     function drawBackground() {
         let pattern = document.createElement("canvas").getContext("2d");
@@ -49,55 +50,101 @@ var Virus;
         crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
         crc2.fillRect(0, 0, canvas.width, canvas.height);
     }
-    function drawCells(_position, _size) {
+    function drawCells() {
         console.log("Zellen");
+        let minR;
+        let maxR;
+        let posX;
+        let posY;
+        let radius;
+        let nCells = (width + height) / 5;
         crc2.beginPath();
         crc2.ellipse(100, 100, 50, 75, Math.PI / 4, 0, 2 * Math.PI);
-        crc2.fillStyle = "blue";
-        crc2.strokeStyle = "yellow";
+        crc2.fillStyle = "#ccccff";
+        crc2.strokeStyle = "#6666ff";
         crc2.fill();
         crc2.stroke();
         // Zellkern zeichnen
         crc2.beginPath();
         crc2.arc(100, 100, 20, 0, 2 * Math.PI);
-        crc2.fillStyle = "#80bfff";
-        crc2.strokeStyle = "white";
+        crc2.fillStyle = "#9999ff";
+        crc2.strokeStyle = "#fff";
         crc2.fill();
         crc2.stroke();
-        for (let drawn = 0; drawn < canvas.width; drawn++) {
+        for (let i = 0; i < nCells; i++) {
+            posX = Math.random() * canvas.width;
+            posY = Math.random() * canvas.height;
+            radius = minR + (Math.random() * (maxR - minR));
+        }
+    }
+    function drawCoronaVirus(_position, _size) {
+        // let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 30, 0, 0, 0);
+        // gradient.addColorStop(0, "#0000ff");
+        // gradient.addColorStop(0.7, "#00cc00");
+        // gradient.addColorStop(1, "#00cc00");
+        // crc2.beginPath();
+        // crc2.arc(0, 0, 40, 0, 2 * Math.PI);
+        // crc2.fillStyle = gradient;
+        // crc2.fill();
+        // crc2.strokeStyle = "black";
+        // crc2.stroke();
+        let r1 = 20;
+        let r2 = 30;
+        let nVirus = 5;
+        let virus = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        virus.arc(0, 0, r2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "red");
+        gradient.addColorStop(1, "orange");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.strokeStyle = "black";
+        crc2.stroke();
+        crc2.fill();
+        for (let drawn = 0; drawn < nVirus; drawn++) {
             crc2.save();
             let x = (Math.random() - 0.5) * _size.x;
             let y = -(Math.random() * _size.y);
             crc2.translate(x, y);
-            crc2.fill();
+            crc2.fill(virus);
             crc2.restore();
         }
         crc2.restore();
     }
-    function drawCoronaVirus(_position, _min, _max) {
-        // let stepMin: number = 10;
-        // let stepMax: number = 150;
-        // let x: number = 0;
-        let radiusVirus = 50;
-        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusVirus);
-        gradient.addColorStop(0, "red");
-        gradient.addColorStop(1, "red");
-        crc2.beginPath();
-        crc2.arc(200, 200, 60, 0, 2 * Math.PI);
-        crc2.fillStyle = gradient;
-        crc2.fill();
-        crc2.closePath();
-    }
-    function drawAntibody() {
-        let radiusAntibody = 50;
-        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusAntibody);
-        gradient.addColorStop(0, "red");
+    function drawAntibody(_position, _size) {
+        // let radiusAntibody: number = 50;
+        // let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusAntibody);
+        // gradient.addColorStop(0, "red");
+        // gradient.addColorStop(1, "white");
+        // crc2.beginPath();
+        // crc2.arc(300, 400, 60, 0, 2 * Math.PI);
+        // crc2.fillStyle = gradient;
+        // crc2.fill();
+        // crc2.closePath()
+        let r1 = 20;
+        let r2 = 30;
+        let nAntibody = 5;
+        let antibody = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        antibody.arc(0, 0, r2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "green");
         gradient.addColorStop(1, "white");
-        crc2.beginPath();
-        crc2.arc(300, 400, 60, 0, 2 * Math.PI);
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.fillStyle = gradient;
+        crc2.strokeStyle = "black";
+        crc2.stroke();
         crc2.fill();
-        crc2.closePath();
+        for (let drawn = 0; drawn < nAntibody; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(antibody);
+            crc2.restore();
+        }
+        crc2.restore();
     }
 })(Virus || (Virus = {}));
 //# sourceMappingURL=script.js.map
--- Aufgaben/Aufgabe_8/script.ts
+++ Aufgaben/Aufgabe_8/script.ts
@@ -1,4 +1,6 @@
 namespace Virus {
+
+
     interface Vector {
         x: number;
         y: number;
@@ -8,6 +10,8 @@ namespace Virus {
     window.addEventListener("load", handleLoad);
     let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
     let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
+    let width: number;
+    let height: number;
 
 
     function resizeWindow(): void {
@@ -20,15 +24,14 @@ namespace Virus {
 
         crc2.fillRect(0, 0, myWidth, myHeight);
 
-        console.log(myWidth, myHeight);
     }
 
     function handleLoad(_event: Event): void {
         resizeWindow();
         drawBackground();
-        drawCells({ x: 500, y: 125 }, { x: 250, y: 75 });
-        drawCoronaVirus({ x: 0, y: 150 }, 30, 100);
-        drawAntibody();
+        drawCells();
+        drawCoronaVirus({x: 200, y: 400}, {x: 70, y: 70});
+        drawAntibody({x: 400, y: 300}, {x: 100, y: 120});
 
     }
 
@@ -69,65 +72,120 @@ namespace Virus {
 
     }
 
-    function drawCells(_position: Vector, _size: Vector): void {
+    function drawCells(): void {
         console.log("Zellen");
+        let minR: 5;
+        let maxR: 10;
+        let posX: number;
+        let posY: number;
+        let radius: number;
+        let nCells: number = (width + height) / 5;
 
         crc2.beginPath();
         crc2.ellipse(100, 100, 50, 75, Math.PI / 4, 0, 2 * Math.PI);
-        crc2.fillStyle = "blue";
-        crc2.strokeStyle = "yellow";
+        crc2.fillStyle = "#ccccff";
+        crc2.strokeStyle = "#6666ff";
         crc2.fill();
         crc2.stroke();
 
         // Zellkern zeichnen
         crc2.beginPath();
         crc2.arc(100, 100, 20, 0, 2 * Math.PI);
-        crc2.fillStyle = "#80bfff";
-        crc2.strokeStyle = "white";
+        crc2.fillStyle = "#9999ff";
+        crc2.strokeStyle = "#fff";
         crc2.fill();
         crc2.stroke();
 
-        for (let drawn: number = 0; drawn < canvas.width; drawn++) {
+        for (let i: number = 0; i < nCells; i++) {
+            posX = Math.random() * canvas.width;
+            posY = Math.random() * canvas.height;
+            radius = minR + (Math.random() * (maxR - minR));
+        }
+
+    }
+
+    function drawCoronaVirus(_position: Vector, _size: Vector): void {
+        // let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 30, 0, 0, 0);
+        // gradient.addColorStop(0, "#0000ff");
+        // gradient.addColorStop(0.7, "#00cc00");
+        // gradient.addColorStop(1, "#00cc00");
+
+        // crc2.beginPath();
+        // crc2.arc(0, 0, 40, 0, 2 * Math.PI);
+        // crc2.fillStyle = gradient;
+        // crc2.fill();
+        // crc2.strokeStyle = "black";
+        // crc2.stroke();
+
+        let r1: number = 20;
+        let r2: number = 30;
+        let nVirus: number = 5;
+        let virus: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        virus.arc(0, 0, r2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "red");
+        gradient.addColorStop(1, "orange");
+        
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.strokeStyle = "black";
+        crc2.stroke();
+        crc2.fill();
+        
+        
+        
+
+        for (let drawn: number = 0; drawn < nVirus; drawn++) {
             crc2.save();
             let x: number = (Math.random() - 0.5) * _size.x;
             let y: number = - (Math.random() * _size.y);
             crc2.translate(x, y);
-            crc2.fill();
+            crc2.fill(virus);
             crc2.restore();
         }
         crc2.restore();
 
-
     }
 
-    function drawCoronaVirus(_position: Vector, _min: number, _max: number): void {
-        // let stepMin: number = 10;
-        // let stepMax: number = 150;
-        // let x: number = 0;
-        let radiusVirus: number = 50;
-        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusVirus);
-        gradient.addColorStop(0, "red");
-        gradient.addColorStop(1, "red");
+    function drawAntibody(_position: Vector, _size: Vector): void {
+        // let radiusAntibody: number = 50;
+        // let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusAntibody);
+        // gradient.addColorStop(0, "red");
+        // gradient.addColorStop(1, "white");
 
-        crc2.beginPath();
-        crc2.arc(200, 200, 60, 0, 2 * Math.PI);
-        crc2.fillStyle = gradient;
-        crc2.fill();
-        crc2.closePath();
-
-    }
-
-    function drawAntibody(): void { 
-        let radiusAntibody: number = 50;
-        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusAntibody);
-        gradient.addColorStop(0, "red");
+        // crc2.beginPath();
+        // crc2.arc(300, 400, 60, 0, 2 * Math.PI);
+        // crc2.fillStyle = gradient;
+        // crc2.fill();
+        // crc2.closePath()
+        let r1: number = 20;
+        let r2: number = 30;
+        let nAntibody: number = 5;
+        let antibody: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        antibody.arc(0, 0, r2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "green");
         gradient.addColorStop(1, "white");
-
-        crc2.beginPath();
-        crc2.arc(300, 400, 60, 0, 2 * Math.PI);
+        
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.fillStyle = gradient;
+        crc2.strokeStyle = "black";
+        crc2.stroke();
         crc2.fill();
-        crc2.closePath();
+        
+        for (let drawn: number = 0; drawn < nAntibody; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x;
+            let y: number = - (Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(antibody);
+            crc2.restore();
+            
+        }
+        crc2.restore();
+
     }
 
 }
